a4e7fcfa9a2e00ef62845b3f5b9884a373fe08b6
SaacNelly <ijnelson7@gmail.com>
2024-11-01 22:08:39 -0500

    Changed Stuff (smirk)

--- CIS-445-Project-Website/MyApp/app.js
+++ CIS-445-Project-Website/MyApp/app.js
@@ -6,7 +6,7 @@ app.use(express.static(path.join(__dirname, '..')));
 
 app.get('/', function(req, res) {
     
-    res.sendFile(path.join(__dirname, '..', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'profile.html'));
 });
 
 app.listen(8080, function() {
